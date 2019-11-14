92f1bfc7e6af813f04bdb60bdb9defefcb6eaafb
Arkady Seletsky <arkady.seletsky@gmail.com>
2019-11-14 12:55:19 +0300

    generate script

---
+++ src/main/resources/generate.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+ARGS="$@"
+
+MAIN_CLASS="com.huawei.GenerationMain"
+
+BIN_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+APP_HOME=`cd $BIN_DIR/.. && pwd`
+echo "App home directory: $APP_HOME"
+echo
+
+CLASSPATH="$APP_HOME/lib"
+
+COMMAND="java -cp $CLASSPATH $MAIN_CLASS $ARGS"
+
+echo "$COMMAND"
