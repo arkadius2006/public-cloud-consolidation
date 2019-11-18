bdc90507106ea3148a0c3e8b48d3b3ae0f00be55
Arkady Seletsky <arkady.seletsky@gmail.com>
2019-11-18 07:52:48 +0300

    same ratio allocation

--- src/main/java/com/huawei/algorithm/SameRatioAllocation.java
+++ src/main/java/com/huawei/algorithm/SameRatioAllocation.java
@@ -3,7 +3,7 @@ package com.huawei.algorithm;
 /**
  * Algorithm assumes hosts and VMs share the same CPU/RAM ratio.
  */
-public class SingleAllocation {
+public class SameRatioAllocation {
 
     // host[a] is a group sharing CPU = 2^a
     // vms[a]  is a group sharing CPU = 2^a
